3812267323c860841e07206f1b0d19bacf8a26a9
七條 毅亮 <shichijotakeaki@MacBook-Pro.local>
2020-09-24 12:50:14 +0900

    [Finish]bookers2-follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,32 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following_user, through: :follower, source: :followed #自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower #自分をフォローしている人
   attachment :profile_image
+
+  #ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  #ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, presence: true
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ devise_for :users
 
 resources :users, only: [:show,:index,:edit,:update]
 
+post 'follow/:id' => 'relationships#follow', as: 'follow' #フォローする
+post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' #フォロー外す
+resources :users, only: :show
+resources :followings
+resources :followers
+
 end
